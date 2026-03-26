450ea5cb692ad56c8a8b4d03ac7db81903179254
Francis Roi Manabat <francisroi.manabat@analog.com>
2026-03-26 18:03:17 +0800

    Added enums PPG/ECG

--- drivers/sensor/adi/max86178/max86178.h
+++ drivers/sensor/adi/max86178/max86178.h
@@ -460,7 +460,449 @@ enum max86178_clk_fine_tune {
 	MAX86178_CLK_FINE_TUNE_SHIFT_NEG_0_2,
 	MAX86178_CLK_FINE_TUNE_SHIFT_COUNT,
 }
+
+enum max86178_bioz_ndiv {
+	MAX86178_BIOZ_NDIV_256 = 0,
+	MAX86178_BIOZ_NDIV_512,
+	MAX86178_BIOZ_NDIV_1024,
+	MAX86178_BIOZ_NDIV_1024_2,
+	MAX86178_BIOZ_NDIV_COUNT,
+}
+
+enum max86178_bioz_kdiv {
+	MAX86178_BIOZ_KDIV_1 = 0,
+	MAX86178_BIOZ_KDIV_2,
+	MAX86178_BIOZ_KDIV_4,
+	MAX86178_BIOZ_KDIV_8,
+	MAX86178_BIOZ_KDIV_16,
+	MAX86178_BIOZ_KDIV_32,
+	MAX86178_BIOZ_KDIV_64,
+	MAX86178_BIOZ_KDIV_128,
+	MAX86178_BIOZ_KDIV_256,
+	MAX86178_BIOZ_KDIV_512,
+	MAX86178_BIOZ_KDIV_1024,
+	MAX86178_BIOZ_KDIV_2048,
+	MAX86178_BIOZ_KDIV_4096,
+	MAX86178_BIOZ_KDIV_8192,
+	MAX86178_BIOZ_KDIV_8192_2,
+	MAX86178_BIOZ_KDIV_8192_3,
+	MAX86178_BIOZ_KDIV_COUNT,
+};
+
+enum max86178_bioz_adc_osr {
+	MAX86178_BIOZ_ADC_OSR_8 = 0,
+	MAX86178_BIOZ_ADC_OSR_16,
+	MAX86178_BIOZ_ADC_OSR_32,
+	MAX86178_BIOZ_ADC_OSR_64,
+	MAX86178_BIOZ_ADC_OSR_128,
+	MAX86178_BIOZ_ADC_OSR_256,
+	MAX86178_BIOZ_ADC_OSR_512,
+	MAX86178_BIOZ_ADC_OSR_1024,
+	MAX86178_BIOZ_ADC_OSR_COUNT,	
+};
+
+enum max86178_bioz_dac_osr {
+	MAX86178_BIOZ_DAC_OSR_32 = 0,
+	MAX86178_BIOZ_DAC_OSR_64,
+	MAX86178_BIOZ_DAC_OSR_128,
+	MAX86178_BIOZ_DAC_OSR_256,
+	MAX86178_BIOZ_DAC_OSR_COUNT,	
+};
+
+enum max86178_ecg_dec_rate {
+	MAX86178_ECG_DEC_RATE_16 = 0,
+	MAX86178_ECG_DEC_RATE_32,
+	MAX86178_ECG_DEC_RATE_64,
+	MAX86178_ECG_DEC_RATE_128,
+	MAX86178_ECG_DEC_RATE_256,
+	MAX86178_ECG_DEC_RATE_512,
+	MAX86178_ECG_DEC_RATE_512_2,
+	MAX86178_ECG_DEC_RATE_512_3,
+	MAX86178_ECG_DEC_RATE_COUNT,
+};
+
+enum max86178_ppg_drva {
+	MAX86178_PPG_DRVA_LED1_DRV = 0,
+	MAX86178_PPG_DRVA_LED2_DRV,
+	MAX86178_PPG_DRVA_LED3_DRV,
+	MAX86178_PPG_DRVA_LED4_DRV,
+	MAX86178_PPG_DRVA_LED5_DRV,
+	MAX86178_PPG_DRVA_LED6_DRV,
+	MAX86178_PPG_DRVA_COUNT,
+};
+
+enum max86178_ppg_drvb {
+	MAX86178_PPG_DRVB_LED1_DRV = 0,
+	MAX86178_PPG_DRVB_LED2_DRV,
+	MAX86178_PPG_DRVB_LED3_DRV,
+	MAX86178_PPG_DRVB_LED4_DRV,
+	MAX86178_PPG_DRVB_LED5_DRV,
+	MAX86178_PPG_DRVB_LED6_DRV,
+	MAX86178_PPG_DRVB_COUNT,
+};
+
+enum max86178_ppg_amb {
+	MAX86178_PPG_AMB_NORMAL_MODE = 0,
+	MAX86178_PPG_AMB_DIRECT_AMB_CONV,
+};
+
+enum max86178_ppg_sinc3_sel {
+	MAX86178_PPG_SINC3_OFF = 0,
+	MAX86178_PPG_SINC3_ON,
+};
+
+enum max86178_ppg_avg_num {
+	MAX86178_PPG_AVG_NUM_1 = 0,
+	MAX86178_PPG_AVG_NUM_2,
+	MAX86178_PPG_AVG_NUM_4,
+	MAX86178_PPG_AVG_NUM_8,
+	MAX86178_PPG_AVG_NUM_16,
+	MAX86178_PPG_AVG_NUM_32,
+	MAX86178_PPG_AVG_NUM_64,
+	MAX86178_PPG_AVG_NUM_128,
+	MAX86178_PPG_AVG_NUM_COUNT,
+};
+
+enum max86178_ppg_filt_sel {
+	MAX86178_PPG_FILT_SEL_CDM = 0,
+	MAX86178_PPG_FILT_SEL_FDM,
+};
+
+enum max86178_ppg_filt2_sel {
+	MAX86178_PPG_FILT2_3RD_ORDER = 0,
+	MAX86178_PPG_FILT2_2ND_ORDER,
+};
+
+enum max86178_ppg_adc_rge {
+	MAX86178_PPG_ADC_RGE_4uA = 0,
+	MAX86178_PPG_ADC_RGE_8uA,
+	MAX86178_PPG_ADC_RGE_16uA,
+	MAX86178_PPG_ADC_RGE_32uA,
+};
+
+enum max86178_ppg_tint {
+	MAX86178_PPG_TINT_14_6us = 0,
+	MAX86178_PPG_TINT_29_2us,
+	MAX86178_PPG_TINT_58_6us,
+	MAX86178_PPG_TINT_117_0us,
+};
+
+enum max86178_ppg_pd_setlng {
+	MAX86178_PPG_PD_SETLNG_7_8us = 0,
+	MAX86178_PPG_PD_SETLNG_11_8us,
+	MAX86178_PPG_PD_SETLNG_15_8us,
+	MAX86178_PPG_PD_SETLNG_23_8us,
+};
+
+enum max86178_led_setlng {
+	MAX86178_PPG_LED_SETLNG_7_7us = 0,
+	MAX86178_PPG_LED_SETLNG_11_7us,
+	MAX86178_PPG_LED_SETLNG_15_7us,
+	MAX86178_PPG_LED_SETLNG_23_7us,
+};
+
+enum max86178_ppg_dac_off {
+	MAX86178_PPG_DAC_OFF_0uA = 0,
+	MAX86178_PPG_DAC_OFF_2uA,
+	MAX86178_PPG_DAC_OFF_4uA,
+	MAX86178_PPG_DAC_OFF_6uA,
+	MAX86178_PPG_DAC_OFF_8uA,
+	MAX86178_PPG_DAC_OFF_10uA,
+	MAX86178_PPG_DAC_OFF_12uA,
+	MAX86178_PPG_DAC_OFF_14uA,
+	MAX86178_PPG_DAC_OFF_16uA,
+	MAX86178_PPG_DAC_OFF_18uA,
+	MAX86178_PPG_DAC_OFF_20uA,
+	MAX86178_PPG_DAC_OFF_22uA,
+	MAX86178_PPG_DAC_OFF_24uA,
+	MAX86178_PPG_DAC_OFF_26uA,
+	MAX86178_PPG_DAC_OFF_28uA,
+	MAX86178_PPG_DAC_OFF_30uA,
+};
+
+enum max86178_ppg_led_rge {
+	MAX86178_PPG_LED_RGE_32mA = 0,
+	MAX86178_PPG_LED_RGE_64mA,
+	MAX86178_PPG_LED_RGE_96mA,
+	MAX86178_PPG_LED_RGE_128mA,
+};
+
+enum max86178_ppg_pd_sel {
+	MAX86178_PD_NOT_SEL = 1,
+	MAX86178_PD_CONN_TO_PPG1,
+	MAX86178_PD_CONN_TO_PPG2,
+};
+
+enum max86178_ppg_pwrdn {
+	MAX86178_PPG_CHAN_ENABLED = 0,
+	MAX86178_PPG_CHAN_PWRDN,
+};
+
+enum max86178_ppg_sync_mode {
+	MAX86178_PPG_INTERNAL_SYNC = 0,
+	MAX86178_PPG_EXTERNAL_SYNC,
+};
+
+enum max86178_smp_ave {
+	MAX86178_SMP_AVE_1 = 0,
+	MAX86178_SMP_AVE_2,
+	MAX86178_SMP_AVE_4,
+	MAX86178_SMP_AVE_8,
+	MAX86178_SMP_AVE_16,
+};
+
+enum max86178_ppg_thresh_meas_sel {
+	MAX86178_PPG_THRESH_DISABLED = 0,
+	MAX86178_PPG_THRESH_MEAS1,
+	MAX86178_PPG_THRESH_MEAS2,
+	MAX86178_PPG_THRESH_MEAS3,
+	MAX86178_PPG_THRESH_MEAS4,
+	MAX86178_PPG_THRESH_MEAS5,
+	MAX86178_PPG_THRESH_MEAS6,
+};
+
+enum max86178_ppg_thresh_chan_sel {
+	MAX86178_PPG_THRESH_PPG_CHAN1 = 0,
+	MAX86178_PPG_THRESH_PPG_CHAN2,
+};
+
+enum max86178_ppg_time_hyst {
+	MAX86178_PPG_TIME_HYST_DISABLED = 0,
+	MAX86178_PPG_TIME_HYST_2_SAMPLES,
+	MAX86178_PPG_TIME_HYST_4_SAMPLES,
+	MAX86178_PPG_TIME_HYST_8_SAMPLES,
+};
+
+enum max86178_ppg_level_hyst {
+	MAX86178_PPG_LEVEL_HYST_DISABLED = 0,
+	MAX86178_PPG_LEVEL_HYST_2_SAMPLES,
+	MAX86178_PPG_LEVEL_HYST_4_SAMPLES,
+	MAX86178_PPG_LEVEL_HYST_8_SAMPLES,
+	MAX86178_PPG_LEVEL_HYST_16_SAMPLES,
+	MAX86178_PPG_LEVEL_HYST_32_SAMPLES,
+	MAX86178_PPG_LEVEL_HYST_64_SAMPLES,
+	MAX86178_PPG_LEVEL_HYST_128_SAMPLES,
+};
+
+enum max86178_ecg_input_pol {
+	MAX86178_ECG_INPUT_POL_NORMAL = 0,
+	MAX86178_ECG_INPUT_POL_INVERTED,
+};
+
+enum max86178_ecg_pga_gain {
+	MAX86178_ECG_PGA_GAIN_1 = 0,
+	MAX86178_ECG_PGA_GAIN_2,
+	MAX86178_ECG_PGA_GAIN_4,
+	MAX86178_ECG_PGA_GAIN_8,
+};
+
+enum max86178_ecg_auto_rec {
+	MAX86178_ECG_AUTO_REC_DISABLED = 0,
+	MAX86178_ECG_AUTO_REC_ENABLED,
+};
+
+enum max86178_en_ecg_fast_rec {
+	MAX86178_EN_ECG_FAST_REC_NORMAL_MODE = 0,
+	MAX86178_EN_ECG_FAST_REC_MANUAL_FAST_REC_MODE,
+	MAX86178_EN_ECG_FAST_REC_AUTO_FAST_REC_MODE,
+};
+
+enum max86178_ecg_cal_freq {
+	MAX86178_ECG_CAL_FREQ_DIV_128 = 0,
+	MAX86178_ECG_CAL_FREQ_DIV_512,
+	MAX86178_ECG_CAL_FREQ_DIV_2048,
+	MAX86178_ECG_CAL_FREQ_DIV_8192,
+	MAX86178_ECG_CAL_FREQ_DIV_32768,
+	MAX86178_ECG_CAL_FREQ_DIV_131072,
+	MAX86178_ECG_CAL_FREQ_DIV_524288,
+	MAX86178_ECG_CAL_FREQ_DIV_2097152,
+};
+
+enum max86178_ecg_cal_duty {
+	MAX86178_ECG_CAL_DUTY_CAL_HIGH = 0,
+	MAX86178_ECG_CAL_DUTY_CAL_50,
+};
+
+enum max86178_ecg_cal_en {
+	MAX86178_ECG_CAL_DISABLED = 0,
+	MAX86178_ECG_CAL_ENABLED,
+};
+
+enum max86178_ecg_open {
+	MAX86178_ECG_PIN_INTERNALLY_CONNECTED = 0,
+	MAX86178_ECG_PIN_INTERNALLY_ISOLATED,
+};
+
+enum max86178_ecg_cal_mode {
+	MAX86178_ECG_CAL_MODE_UNIPOLAR = 0,
+	MAX86178_ECG_CAL_MODE_BIPOLAR,
+};
+
+enum max86178_ecg_cal_mag {
+	MAX86178_ECG_CAL_MAG_0_5mV = 0,
+	MAX86178_ECG_CAL_MAG_1mV,
+};
+
+enum max86178_ecg_cal_sel{
+	MAX86178_ECG_CAL_SEL_NO_CAL = 0,
+	MAX86178_ECG_CAL_SEL_CONN_VMID_ECG,
+	MAX86178_ECG_CAL_SEL_CONN_VCALP,
+	MAX86178_ECG_CAL_SEL_CONN_VCALN,
+};
+
+enum max86178_ecg_lon {
+	MAX86178_ECG_LON_DISABLED = 0,
+	MAX86178_ECG_LON_ENABLED,
+};
+
+enum max86178_ecg_loff_en {
+	MAX86178_ECG_LOFF_DISABLED = 0,
+	MAX86178_ECG_LOFF_ENABLED,
+};
+
+enum max86178_ecg_loff_mode {
+	MAX86178_ECG_DC_LEAD_OFF_DETECTION_MODE = 0,
+	MAX86178_ECG_AC_LEAD_OFF_DETECTION_MODE,
+};
+
+enum max86178_ecg_loff_freq {
+	MAX86178_ECG_LOFF_FREQ_DISABLED = 0,
+	MAX86178_ECG_LOFF_FREQ_4Hz,
+	MAX86178_ECG_LOFF_FREQ_8Hz,
+	MAX86178_ECG_LOFF_FREQ_16Hz,
+	MAX86178_ECG_LOFF_FREQ_32Hz,
+	MAX86178_ECG_LOFF_FREQ_64Hz,
+	MAX86178_ECG_LOFF_FREQ_128Hz,
+	MAX86178_ECG_LOFF_FREQ_256Hz,
+};
+
+enum max86178_ecg_loff_ipol {
+	MAX86178_ECG_LOFF_IPOL_NON_INVERTED = 0,
+	MAX86178_ECG_LOFF_IPOL_INVERTED,
+};
+
+enum max86178_ecg_loff_imag {
+	MAX86178_ECG_LOFF_IMAG_0nA = 0,
+	MAX86178_ECG_LOFF_IMAG_5nA,
+	MAX86178_ECG_LOFF_IMAG_10nA,
+	MAX86178_ECG_LOFF_IMAG_20nA,
+	MAX86178_ECG_LOFF_IMAG_50nA,
+	MAX86178_ECG_LOFF_IMAG_100nA,
+	MAX86178_ECG_LOFF_IMAG_200nA,
+	MAX86178_ECG_LOFF_IMAG_400nA,
+};
+
+enum max86178_ecg_rbias_value {
+	MAX86178_ECG_RBIAS_50M = 0,
+	MAX86178_ECG_RBIAS_100M,
+	MAX86178_ECG_RBIAS_200M,
+};
+
+enum max86178_en_ecg_rbias{
+	MAX86178_EN_ECG_RBIAS_DISCONNECTED = 0,
+	MAX86178_EN_ECG_RBIAS_CONNECTED,
+};
+
+enum max86178_ecg_rld_en {
+	MAX86178_RLD_DISABLED = 0,
+	MAX86178_RLD_ENABLED,
+};
+
+enum max86178_ecg_rld_mode {
+	MAX86178_RLD_MODE_OPEN_LOOP = 0,
+	MAX86178_RLD_MODE_CLOSED_LOOP,
+};
+
+enum max86178_ecg_rld_rbias {
+	MAX86178_RLD_RBIAS_VMID = 0,
+	MAX86178_RLD_RBIAS_VRLD,
+};
+
+enum max86178_ecg_en_rld_oor {
+	MAX86178_EN_RLD_OOR_DISABLED = 0,
+	MAX86178_EN_RLD_OOR_ENABLED,
+};
+
+enum max86178_ecg_actv_cm {
+	MAX86178_ECG_ACTV_CM_DISABLED = 0,
+	MAX86178_ECG_ACTV_CM_ENABLED,
+};
+
+enum max86178_ecg_rld_gain {
+	MAX86178_RLD_GAIN_12 = 0,
+	MAX86178_RLD_GAIN_24,
+	MAX86178_RLD_GAIN_48,
+	MAX86178_RLD_GAIN_97,
+};
+
+enum max86178_rld_ext_res {
+	MAX86178_RLD_EXT_RES_INTERNAL = 0,
+	MAX86178_RLD_EXT_RES_EXTERNAL,
+};
+
+enum max86178_bioz_dac_osr {
+	MAX86178_BIOZ_DAC_OSR_32 = 0,
+	MAX86178_BIOZ_DAC_OSR_64,
+	MAX86178_BIOZ_DAC_OSR_128,
+	MAX86178_BIOZ_DAC_OSR_256,
+};
+
+enum max86178_bioz_adc_osr {
+	MAX86178_BIOZ_ADC_OSR_8 = 0,
+	MAX86178_BIOZ_ADC_OSR_16,
+	MAX86178_BIOZ_ADC_OSR_32,
+	MAX86178_BIOZ_ADC_OSR_64,
+	MAX86178_BIOZ_ADC_OSR_128,
+	MAX86178_BIOZ_ADC_OSR_256,
+	MAX86178_BIOZ_ADC_OSR_512,
+	MAX86178_BIOZ_ADC_OSR_1024,
+};
+
+enum max86178_ecg_bioz_bg_en {
+	MAX86178_ECG_BIOZ_BG_EN_DISABLED = 0,
+	MAX86178_ECG_BIOZ_BG_EN_ENABLED,
+};
+
+enum max86178_bioz_en {
+	MAX86178_BIOZ_EN_DISABLED = 0,
+	MAX86178_BIOZ_EN_I_CHANNEL,
+	MAX86178_BIOZ_EN_Q_CHANNEL,
+	MAX86178_BIOZ_EN_UTILITY,
+};
+
+enum max86178_bioz_dhpf {
+	MAX86178_BIOZ_DHPF_BYPASS = 0,
+	MAX86178_BIOZ_DHPF_0_00025,
+	MAX86178_BIOZ_DHPF_0_002,
+};
+
+enum max86178_bioz_dlpf {
+	MAX86178_BIOZ_DLPF_BYPASS = 0,
+	MAX86178_BIOZ_DLPF_0_005,
+	MAX86178_BIOZ_DLPF_0_02,
+	MAX86178_BIOZ_DLPF_0_08,
+	MAX86178_BIOZ_DLPF_0_25,
+};
+
+enum max86178_en_bioz_thresh {
+	MAX86178_EN_BIOZ_THRESH_DISABLED = 0,
+	MAX86178_EN_BIOZ_THRESH_ENABLED,
+};
+
+enum max86178_bioz_ext_res {
+	MAX86178_BIOZ_EXT_RES_INTERNAL = 0,
+	MAX86178_BIOZ_EXT_RES_EXTERNAL,
+};
+
+enum max86178_bioz_vdrv_mag {
+	MAX86178_BIOZ_VDRV_MAG_50mV = 0,
+	MAX86178_BIOZ_VDRV_MAG_100mV,
+	MAX86178_BIOZ_VDRV_MAG_250mV,
+	MAX86178_BIOZ_VDRV_MAG_500mV,
+};
+
 /**
+ * 
  * @brief MAX86178 bus configuration
  *
  */
@@ -489,21 +931,20 @@ typedef int (*max86178_reg_access_fn)(const struct device *dev, bool read, uint8
 
 /* Clock Configuration Structs */
 struct max86178_clk_ppg_cfg {
-	uint8_t frame_rate;
+	uint16_t ppg_fr_clk_div : 15;
 };
 
 struct max86178_clk_ecg_cfg {
 	uint8_t ecg_fdiv : 3;
 	uint16_t ecg_ndiv : 11;
-	uint8_t ecg_dec_rate : 3;
+	enum max86178_ecg_dec_rate ecg_dec_rate;
 };
 
 struct max86178_clk_bioz_cfg {
-	uint8_t resp_en : 1;
-	uint8_t bioz_kdiv : 4;
-	uint8_t bioz_ndiv : 2;
-	uint8_t bioz_adc_osr : 3;
-	uint8_t bioz_dac_osr : 3;
+	enum max86178_bioz_kdiv bioz_kdiv;
+	enum max86178_bioz_ndiv bioz_ndiv;
+	enum max86178_bioz_adc_osr bioz_adc_osr;
+	enum max86178_bioz_dac_osr bioz_dac_osr;
 };
 
 struct max86190_clk_osc_cfg {
@@ -522,67 +963,207 @@ struct max86178_clk_cfg {
 
 /* PPG Structs */
 struct max86178_ppg_meas_cfg {
-	uint8_t drva : 3;
-	uint8_t drvb : 3;
-	bool amb_en : 1;
-	uint8_t avg_num : 3;
-	bool filt_sel : 1;
-	bool filt2_sel : 1;
-	bool sinc3_sel : 1;
-	uint8_t ppg1_dac_off : 4;
-	uint8_t ppg1_adc_rge : 2;
-	uint8_t ppg2_dac_off : 4;
-	uint8_t ppg2_adc_rge : 2;
-	uint8_t tint : 2;
-	uint8_t pd_setlng : 2;
-	uint8_t pd1_sel : 2;
-	uint8_t pd2_sel : 2;
-	uint8_t pd3_sel : 2;
-	uint8_t pd4_sel : 2;
-	uint8_t led_rge : 2;
-	uint8_t led_setlng : 2;
+	enum max86178_ppg_drva drva;
+	enum max86178_ppg_drvb drvb;
+	enum max86178_ppg_amb amb_mode;
+	enum max86178_ppg_avg_num avg_num;
+	enum max86178_ppg_sinc3_sel sinc3_sel;
+	enum max86178_ppg_filt_sel filt_sel;
+	enum max86178_ppg_filt2_sel filt2_sel;
+	enum max86178_ppg_dac_off ppg1_dac_off;
+	enum max86178_ppg_adc_rge ppg1_adc_rge;
+	enum max86178_ppg_dac_off ppg2_dac_off;
+	enum max86178_ppg_adc_rge ppg2_adc_rge;
+	enum max86178_ppg_tint tint;
+	enum max86178_ppg_pd_setlng pd_setlng;
+	enum max86178_led_setlng led_setlng;
+	enum max86178_ppg_led_rge led_rge;
+	enum max86178_ppg_pd_sel pd1_sel;
+	enum max86178_ppg_pd_sel pd2_sel;
+	enum max86178_ppg_pd_sel pd3_sel;
+	enum max86178_ppg_pd_sel pd4_sel;
+	uint8_t drva_pa;
+	uint8_t drvb_pa;
+};
+
+struct max86178_ppg_threshold_cfg {
+	enum max86178_ppg_thresh_meas_sel thresh1_meas_sel;
+	enum max86178_ppg_thresh_meas_sel thresh2_meas_sel;
+	enum max86178_ppg_thresh_chan_sel thresh1_chan_sel;
+	enum max86178_ppg_thresh_chan_sel thresh2_chan_sel;
+	enum max86178_ppg_time_hyst time_hyst;
+	enum max86178_ppg_level_hyst level_hyst;
+	uint16_t thresh1_hi;
+	uint16_t thresh1_lo;
+	uint16_t thresh2_hi;
+	uint16_t thresh2_lo;
 };
 
 struct max86178_ppg_cfg {
 	bool meas_en[6];
-	bool ppg1_pwrdn;
-	bool ppg2_pwrdn;
-	bool ppg_sync_mode;
+	enum max86178_ppg_pwrdn ppg1_pwrdn;
+	enum max86178_ppg_pwrdn ppg2_pwrdn;
+	enum max86178_ppg_sync_mode ppg_sync_mode;
 	bool prox_data_en;
 	bool prox_auto_en;
 	bool alc_disable;
 	bool collect_raw_data;
 	bool meas1_config_sel;
-	uint8_t smp_ave: 3;
+	enum max86178_smp_ave smp_ave;
 	struct max86178_ppg_meas_cfg meas_cfg[6];
+	struct max86178_ppg_threshold_cfg threshold_cfg;
 };
 
 /* ECG Structs */
 struct max86178_ecg_setup {
 	bool ecg_en;
-	bool ecg_input_pol;
-	uint8_t ecg_pga_gain : 3;
+	enum max86178_ecg_input_pol ecg_input_pol;
+	enum max86178_ecg_pga_gain ecg_pga_gain;
 	uint8_t ecg_ina_rge : 2;
 	uint8_t ecg_ina_gain : 3;
 	bool ecg_imp_hi;
-	bool ecg_auto_rec;
+	enum max86178_ecg_auto_rec ecg_auto_rec;
 	uint8_t ecg_mux_sel : 2;
-	uint8_t en_ecg_fast_rec : 2;
+	enum max86178_en_ecg_fast_rec en_ecg_fast_rec;
 	uint8_t ecg_fast_rec_thres : 6;
 };
 
 struct max86178_ecg_calibration {
 	uint16_t ecg_cal_high : 11;
-	uint8_t ecg_freq : 3;
-	bool ecg_cal_duty;
-	bool ecg_cal_en;
-	bool ecg_open_p;
-	bool ecg_open_n;
-	bool ecg_cal_mode;
-	bool ecg_cal_mag;
-	uint8_t ecg_cal_p_sel : 2;
-	uint8_t ecg_cal_n_sel : 2;
+	enum max86178_ecg_cal_freq ecg_freq;
+	enum max86178_ecg_cal_duty ecg_cal_duty;
+	enum max86178_ecg_cal_en ecg_cal_en;
+	enum max86178_ecg_open ecg_open_p;
+	enum max86178_ecg_open ecg_open_n;
+	enum max86178_ecg_cal_mode ecg_cal_mode;
+	enum max86178_ecg_cal_mag ecg_cal_mag;
+	enum max86178_ecg_cal_sel ecg_cal_p_sel;
+	enum max86178_ecg_cal_sel ecg_cal_n_sel;
 };
+
+struct max86178_ecg_lead_detect {
+	enum max86178_ecg_lon en_ecg_lon;
+	enum max86178_ecg_loff_en en_ecg_loff;
+	enum max86178_ecg_loff_mode ecg_loff_mode;
+	enum max86178_ecg_loff_freq ecg_loff_freq;
+	enum max86178_ecg_loff_ipol ecg_loff_ipol;
+	enum max86178_ecg_loff_imag ecg_loff_current_mag;
+	uint8_t ecg_loff_thresh : 4;
+};
+
+struct max86178_lead_bias {
+	enum max86178_ecg_rbias_value ecg_rbias_value;
+	enum max86178_en_ecg_rbias en_ecg_rbias_p;
+	enum max86178_en_ecg_rbias en_ecg_rbias_n;
+};
+
+struct max86178_rld_cfg {
+	enum max86178_ecg_rld_en rld_en;
+	enum max86178_ecg_rld_mode rld_mode;
+	enum max86178_ecg_rld_rbias rld_rbias;
+	enum max86178_ecg_en_rld_oor en_rld_oor;
+	enum max86178_ecg_actv_cm actv_cm_p;
+	enum max86178_ecg_actv_cm actv_cm_n;
+	enum max86178_ecg_rld_gain rld_gain;
+	bool rld_ext_res;
+	bool rld_sel_ecg;
+	uint8_t rld_bw : 2;
+	uint8_t body_bias_dac : 4;
+};
+
+struct max86178_ecg_cfg {
+	struct max86178_ecg_setup setup;
+	struct max86178_ecg_calibration calibration;
+	struct max86178_ecg_lead_detect lead_detect;
+	struct max86178_lead_bias lead_bias;
+	struct max86178_rld_cfg rld_cfg;
+};
+
+
+struct max86178_bioz_setup {
+	enum max86178_bioz_dac_osr bioz_dac_osr;
+	enum max86178_bioz_adc_osr bioz_adc_osr;
+	enum max86178_ecg_bioz_bg_en ecg_bioz_bg_en;
+	enum max86178_bioz_en bioz_en;
+	enum max86178_bioz_dhpf bioz_dhpf;
+	enum max86178_bioz_dlpf bioz_dlpf;
+	enum max86178_en_bioz_thresh en_bioz_thresh;
+	enum max86178_bioz_ext_res bioz_ext_res;
+	uint8_t bioz_vdrv_mag : 2;
+	uint8_t bioz_idrv_rge : 2;
+	uint8_t bioz_drv_mode : 2;
+	bool en_util_mode;
+	bool bioz_dc_code_sel;
+	uint8_t bioz_dc_dac_code : 7;
+	uint8_t bioz_ahpf : 4;
+	bool bioz_ina_mode;
+	bool bioz_dm_dis;
+	bool bioz_gain : 2;
+	bool bioz_ext_cap;
+	bool bioz_dc_restore;
+	bool bioz_drv_reset;
+	bool bioz_dac_reset;
+	uint8_t bioz_amp_rge : 2;
+	uint8_t bioz_amp_bw : 2;
+	bool rld_sel_bioz;
+	bool rld_drv;
+	bool bioz_cmres_dis;
+	bool bioz_stbyon;
+	bool bioz_ipol;
+	bool bioz_fast;
+	bool bioz_ina_chop_en;
+	bool bioz_ch_fsel;
+	uint8_t bioz_lo_thresh;
+	uint8_t bioz_hi_thresh;
+};
+
+struct max86178_bioz_calibration {
+	uint8_t bmux_rsel : 2;
+	bool bmux_bist_en;
+	bool connect_cal_only;
+	bool bioz_mux_en;
+	bool bioz_cal_en;
+	uint8_t bmux_gsr_rsel : 2;
+	bool gsr_load_en;
+	bool en_ext_inload;
+	bool en_int_inload;
+	uint8_t bip_assign : 2;
+	uint8_t bin_assign : 2;
+	uint8_t drvp_assign : 2;
+	uint8_t drvn_assign : 2;
+};
+
+struct max86178_bioz_lead_detect {
+	bool en_bioz_lon;
+	bool en_bioz_loff;
+	bool en_ext_bioz_loff;
+	bool bioz_loff_ipol;
+	uint8_t bioz_loff_current_mag : 3;
+	bool resp_cg_mag4x;
+	uint8_t resp_cg_mag : 3;
+	uint8_t bioz_loff_thresh : 4;
+};
+
+struct max86178_lead_bias {
+	bool en_bioz_rbias_p;
+	bool en_bioz_rbias_n;
+	uint8_t bioz_rbias_value : 2;
+};
+
+struct max86178_bioz_cfg {
+	struct max86178_bioz_setup setup;
+	struct max86178_bioz_calibration calibration;
+	struct max86178_bioz_lead_detect lead_detect;
+	struct max86178_lead_bias lead_bias;
+};
+
+struct max86178_resp_setup {
+	bool resp_en;
+	uint8_t cg_mode : 2;
+	uint8_t cg_chop_clk : 2;
+	uint8_t cg_lpf_duty : 3;
+};
+
 
 /**
  * @brief MAX86178 driver data structure
